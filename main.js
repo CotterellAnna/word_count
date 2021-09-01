1c47aa017370cd852114080a6acb3e862db4af38
CotterellAnna <cotterellanna@gmail.com>
2021-09-01 09:01:01 +0100

    correct word count

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function count_(){
     let text_box = document.getElementById('text_box').value;
     count.value = 0;
     for(let i=0; i<text_box.length; i++){
-        if(text_box[i] == ' ' && text_box[text_box.length] !== ' '){
-            count.value++
+        if(text_box[i]==' ' && text_box[i+1] != ' '){
+            count.value++;
         }
     }
 }
